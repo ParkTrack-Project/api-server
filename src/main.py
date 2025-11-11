d012c866b951b4b6cf41e2ee915668f7b87fbb5d
Egor Stolbov <egor100lbov@gmail.com>
2025-11-11 20:15:11 +0300

    Change: DBManager now works on sqlalchemy instead of sqlite3

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from api.api import PublicAPI, URL
 from db_manager.db_manager import DBManager
 
 def main():
-    api_server = PublicAPI(db_manager=DBManager("mock"))
+    api_server = PublicAPI(DBManager())
     api_server.run(URL(host="127.0.0.1", port=8000))
 
 if __name__ == "__main__":

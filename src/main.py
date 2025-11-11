e5205e6572d12acc6f960e160112ed337a096dac
Egor Stolbov <egor100lbov@gmail.com>
2025-11-11 20:39:33 +0300

    Fix: host and port

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from db_manager.db_manager import DBManager
 
 def main():
     api_server = PublicAPI(DBManager())
-    api_server.run(URL(host="parktrack-api.nawinds.dev/", port="443"))
+    api_server.run(URL(host="127.0.0.1", port="8000"))
 
 if __name__ == "__main__":
     main()

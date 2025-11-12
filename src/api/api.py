804708fd2e799e8346b7509bb0fa08017aec15e3
Egor Stolbov <egor100lbov@gmail.com>
2025-11-12 23:26:17 +0300

    Fix: quick fix

--- src/api/api.py
+++ src/api/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-from models import CreateCamera
+from .models import CreateCamera
 
 class URL(BaseModel):
     port: str

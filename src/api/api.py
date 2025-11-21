6c178a14d5c5bde660385dbc7e1b52a1f12f46be
Egor Stolbov <egor100lbov@gmail.com>
2025-11-21 19:31:14 +0300

    refactor(api): remove unused update schema imports

--- src/api/api.py
+++ src/api/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, status, Request
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-from .models import CreateCamera, CreateZone, UpdateCamera, UpdateZone
+from .models import CreateCamera, CreateZone
 
 import json
 

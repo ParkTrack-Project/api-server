b2b19b3b9f7fd38f7488b2231cc711c30b24c49e
Egor Stolbov <egor100lbov@gmail.com>
2025-11-21 11:07:49 +0300

    Add: GET /cameras

--- src/main.py
+++ src/main.py
@@ -2,8 +2,13 @@ from api.api import PublicAPI, URL
 from db_manager.db_manager import DBManager
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def main():
-    api_server = PublicAPI(DBManager())
+    print(os.getenv("DB_CONNECTION_URL"), os.getenv("HOST"), os.getenv("PORT"))
+    api_server = PublicAPI(DBManager(os.getenv("DB_CONNECTION_URL")))
     api_server.run(URL(host=os.getenv("HOST"), port=os.getenv("PORT")))
 
 if __name__ == "__main__":

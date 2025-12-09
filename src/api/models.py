8f8a7e385d79cd9d472a8f89f6bb5ba44f7a59e0
Egor Stolbov <egor100lbov@gmail.com>
2025-12-09 22:55:18 +0300

    Fix: Fix?

--- src/api/models.py
+++ src/api/models.py
@@ -1,72 +1,13 @@
 from typing import List, Dict, TypedDict, Any, Optional, Literal
 from pydantic import BaseModel, field_validator, Field
 import json
-
-# class CreateCamera(BaseModel):
-#     title: str
-#     source: str
-#     image_width: int
-#     image_height: int
-#     calib: Any
-#     latitude: float
-#     longitude: float
-    
-#     @field_validator('title')
-#     @classmethod
-#     def validate_title(cls, title):
-#         if len(title) < 1 or len(title) > 200:
-#             raise ValueError(f"Invalid camera title: {title}")
-#         return title
-
-#     @field_validator('source')
-#     @classmethod
-#     def validate_source(cls, source):
-#         return source
-    
-#     @field_validator('latitude')
-#     @classmethod
-#     def validate_latitude(cls, latitude):
-#         if latitude > 90 or latitude < -90:
-#             raise ValueError(f"Invalid latitude value: {latitude}")
-#         return latitude
-    
-#     @field_validator('longitude')
-#     @classmethod
-#     def validate_longitude(cls, longitude):
-#         if longitude > 180 or longitude < -180:
-#             raise ValueError(f"Invalid longitude value: {longitude}")
-#         return longitude
-    
-#     @field_validator('image_width')
-#     @classmethod
-#     def validate_image_width(cls, image_width):
-#         if image_width <= 0: 
-#             raise ValueError(f"Invalid image_width value: {image_width}")
-#         return image_width
-    
-#     @field_validator('image_height')
-#     @classmethod
-#     def validate_image_height(cls, image_height):
-#         if image_height <= 0: 
-#             raise ValueError(f"Invalid image_height value: {image_height}")
-#         return image_height
-    
-#     @field_validator('calib')
-#     @classmethod
-#     def validate_calib(cls, calib):
-#         if calib is not None:
-#             try:
-#                 json.dumps(calib)
-#             except:
-#                 raise ValueError(f"Invalid calibration data")
-#         return calib
     
 class CameraBase(BaseModel):
     title: Optional[str] = Field(None, min_length=3, max_length=120)
     source: Optional[str] = Field(None, max_length=250)
     image_width: Optional[int] = Field(None, gt=0)
     image_height: Optional[int] = Field(None, gt=0)
-    calib: Any
+    calib: Optional[Any] = None
     latitude: Optional[float] = Field(None, ge=-90, le=90)
     longitude: Optional[float] = Field(None, ge=-180, le=180)
 

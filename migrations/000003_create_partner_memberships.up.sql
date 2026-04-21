a689eb904565aca764951a1039020638c4977cf4
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2026-04-21 13:34:01 +0300

    fix: typo in column "user_id" in migration 3

--- migrations/000003_create_partner_memberships.up.sql
+++ migrations/000003_create_partner_memberships.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS partner_memberships (
     partner_membership_id SERIAL PRIMARY KEY,
-    uesr_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
     partner_id INTEGER REFERENCES partners(partner_id) ON DELETE CASCADE,
     user_role VARCHAR(50) NOT NULL,
     read_scope VARCHAR(50) DEFAULT 'own',

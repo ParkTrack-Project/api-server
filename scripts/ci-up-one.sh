260fdea227728b06572907c501f5eec09cbbb718
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2026-05-05 13:06:37 +0300

    feat: provide staircase step scripts with database connection url

--- scripts/ci-up-one.sh
+++ scripts/ci-up-one.sh
@@ -5,7 +5,11 @@ MIGRATION_FILE="$1"
 
 VERSION=$(basename "$MIGRATION_FILE" | sed 's/^0*//' | cut -d'_' -f1)
 
-TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}/${POSTGRES_TEST_DB}?sslmode=disable"
+if [ $# -ge 2 ]; then
+  TEST_DB_URL="$2"
+else
+  TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}/${POSTGRES_TEST_DB}?sslmode=disable"
+fi
 
 migrate -path ./migrations -database "$TEST_DB_URL" goto "$VERSION"
 

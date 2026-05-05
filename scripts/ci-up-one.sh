43f64174f5e05202bac1a5d5ff9ff38efc88a72c
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2026-05-05 09:49:07 +0300

    feat: update ci-up-one.sh to allow cahnge of database host

--- scripts/ci-up-one.sh
+++ scripts/ci-up-one.sh
@@ -5,7 +5,7 @@ MIGRATION_FILE="$1"
 
 VERSION=$(basename "$MIGRATION_FILE" | sed 's/^0*//' | cut -d'_' -f1)
 
-TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_TEST_DB}?sslmode=disable"
+TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}/${POSTGRES_TEST_DB}?sslmode=disable"
 
 migrate -path ./migrations -database "$TEST_DB_URL" goto "$VERSION"
 

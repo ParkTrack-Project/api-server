62721fd28bdc558975647cd9b22606068019167c
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2026-05-05 13:27:41 +0300

    feat: centralize database urls creation

--- scripts/ci-down-one.sh
+++ scripts/ci-down-one.sh
@@ -5,7 +5,7 @@ set -eo
 if [ $# -ge 2 ]; then
   TEST_DB_URL="$2"
 else
-  TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}/${POSTGRES_TEST_DB}?sslmode=disable"
+  TEST_DB_URL="${DATABASE_HOST_URL:="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}"}/${POSTGRES_TEST_DB}?sslmode=disable"
 fi
 
 migrate -path ./migrations -database "$TEST_DB_URL" down 1

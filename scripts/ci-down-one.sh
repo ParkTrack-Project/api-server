2664be43c850726e73a7934a53966d4227b8f2ab
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2026-05-05 09:49:45 +0300

    feat: update ci-down-one.sh to allow change postgres host

--- scripts/ci-down-one.sh
+++ scripts/ci-down-one.sh
@@ -2,7 +2,7 @@
 
 set -eo
 
-TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_TEST_DB}?sslmode=disable"
+TEST_DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:${POSTGRES_PORT:-5432}/${POSTGRES_TEST_DB}?sslmode=disable"
 
 
 migrate -path ./migrations -database "$TEST_DB_URL" down 1
